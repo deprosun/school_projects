9f84f8f3a1dee9b63944e24351f554dacf96ae35
deprosun <karang90@gmail.com>
2015-04-23 10:53:49 -0400

    html css

--- Web Development/Javascript/Constructor_Functions.js
+++ Web Development/Javascript/Constructor_Functions.js
@@ -13,12 +13,6 @@ var elf = new Hero("elf","9","9","9");
 var heroes = [dwarf, barbarian, elf];
 
 for(i = 0; i < heroes.length; i++){
-  // document.write("<h3>"+heroes[i].heroType.toUpperCase()+"</h3>");
-  // document.write("Armor: "+ heroes[i].armor +"<br />");
-  // document.write("Strength: "+ heroes[i].strength +"<br />");
-  // document.write("Attack: "+ heroes[i].attack +"<br />");
-  // document.write("<br />");
-
   for(var x in heroes[i]){
     document.write(x + " : " + heroes[i][x] + "<br />");
   }
