deb9bf546b759d782c17521a226f3407ccbb33bd
deprosun <karang90@gmail.com>
2015-05-05 18:05:30 -0400

    removing web dev folder

--- Web Development/socially/.meteor/local/build/programs/web.browser/app/template.index.js
+++
@@ -1,8 +0,0 @@
-(function(){
-Template.body.addContent((function() {
-  var view = this;
-  return HTML.Raw('<div ng-app="socially" ng-include="\'index.ng.html\'"></div>');
-}));
-Meteor.startup(Template.body.renderToDocument);
-
-})();
